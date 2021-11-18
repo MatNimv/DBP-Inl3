f8762f0bebd5f1d1a0f27368396737456ea51048
qlaramarie <claramarielund@gmail.com>
2021-11-18 21:34:47 +0100

    Update read.php

--- users/read.php
+++ users/read.php
@@ -1,6 +1,6 @@
 <?php
 header( "Content-type: application/json" );
-require_once "functions.php";
+require_once "../functions.php";
 
 $allUsers = loadJson("users.json");
 $allPets = loadJson("../pets.json")
@@ -9,6 +9,7 @@ $allPets = loadJson("../pets.json")
 
 <?php
 $requestMethod = $_SERVER["REQUEST_METHOD"];
+if ($requestMethod === "GET"){
 //first_name
 if (isset($_GET["first_name"])) {
     foreach ($allUsers as $user => $user) {
@@ -17,5 +18,28 @@ if (isset($_GET["first_name"])) {
         }
     }
 }
+if (isset($_GET["limit"])) {
+    $limit = $_GET["limit"];
+    $slicedUsers = array_slice($users, 0, $limit);
+    send($slicedUsers);
+}
+
+
+if (isset($_GET["ids"])) {
+    $ids = explode(",", $_GET["ids"]);
+    $usersById = [];
+
+    foreach ($users as $user) {
+        if (in_array($user["id"], $ids)) {
+            $usersById[] = $user;
+        }
+    }
+
+    send($usersById);
+}
+
+send($users); 
+}
+
 
 ?>
