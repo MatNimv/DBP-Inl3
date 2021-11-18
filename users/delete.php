17e8c7ac736041ce89f2a730d021f0053b2da92f
MatNimv <matilda.nimvik@gmail.com>
2021-11-18 11:52:58 +0100

    updated delete.php
    Förtydligande med kommentarer. Adderade felmeddelanden och statuskoder.

--- users/delete.php
+++ users/delete.php
@@ -1,36 +1,42 @@
 <?php
 require_once "../functions.php";
 //denna sidan tar bort EN användare. 
-//Tar emot {id}, 
-//skickar iväg användaren som togs bort.
+//Input: {id}, 
+//Output: id samt hela användaren.
+//   {
+//       id: 
+//       first_name:
+//       last_name:
+//       gender:
+//       pet:
+//   }
 
 //tar emot inputen som användaren har gett i Insomnia.
 //vilket är endast id.
 $data = file_get_contents("php://input");
 //decodar innehållet till en assossiativ array.
 $requestData = json_decode($data, true);
-//ALLA användare från users.json
-$allUsers = loadJSON("users.json");
 
 //variabel för metoden.
 $method = $_SERVER["REQUEST_METHOD"];
 //variabeln för content type.
 $contentType = $_SERVER["CONTENT_TYPE"];
 
-//ALLA användare från databasen.
+//får ut ALLA användare från databasen.
 $allUsers = loadJSON("users.json");
 
 //allt utförs ENDAST om metoden är DELETE.
 if ($method === "DELETE"){
+    //utförs endast om contentype är json
     if ($contentType === "application/json"){
         $found = false;
         $id = $requestData["id"];
 
         $foundUser = null;
 
-        if (!isset($id)){
+        if (!isset($id)){ //om id inte ens finns i URl.
             sendJson([
-                "message" => "ID finns inte i requesten."
+                "message" => "Bad Request. ID must be included."
             ],400);
         }
 
@@ -42,20 +48,26 @@ if ($method === "DELETE"){
                 break;
             } 
         }
-        if ($found == false){
+        if ($found == false){ //om id inte matchar med den requestade.
             sendJson([
-                "message" => "ID:$id does not exist"
+                "message" => "ID:$id was not found."
             ], 404);
         }
 
+        //sparar den "uppdaterade" users.json filen.
         saveJson("users.json", $allUsers);
+        //meddelar vilket id samt hela användaren som togs bort
         sendJson([
             "message" => "You removed this user ID:$id",
             "user" => $user]);
-        } else {
+        } else { //om contenttype inte är application/json.
             sendJson([
-                "message" => "Content type must me JSON."
+                "message" => "Content type must be JSON."
             ], 400);
         }
+    } else { //alla andra metoder än DELETE.
+        sendJson([
+            "message" => "Method is not allowed."
+        ], 405);
     }
 ?>
