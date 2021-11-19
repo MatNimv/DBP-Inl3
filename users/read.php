1d50dad5bdbd1c2a0f6b8f7f0b26400669449823
MatNimv <matilda.nimvik@gmail.com>
2021-11-19 15:24:28 +0100

    read.php
    adderade age-GET parametern. Nu kan man få alla användare med en specifik ålder.

--- users/read.php
+++ users/read.php
@@ -7,43 +7,60 @@ $allPets = loadJson("../pets.json");
 <?php
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 if ($requestMethod === "GET") {
-    //first_name
-    if (isset($_GET["first_name"])) {
-        foreach ($allUsers as $user => $user) {
-            if ($user["first_name"] == $_GET["first_name"]) {
-                sendJson($allUsers[$user]);
-                exit();
-            
+        //first_name
+        if (isset($_GET["first_name"])) {
+            foreach ($allUsers as $user => $user) {
+                if ($user["first_name"] == $_GET["first_name"]) {
+                    sendJson($allUsers[$user]);
+                    exit();
+                
+                }
             }
         }
-    }
-}
-if (isset($_GET["limit"])) {
-    $limit = $_GET["limit"];
-    $slicedUsers = array_slice($allUsers, 0, $limit);
-    sendJson($slicedUsers);
-    exit();
-   
-}
 
-
-if (isset($_GET["id"])) {
-    $ids = explode(",", $_GET["id"]);
-    $usersById = [];
-
-    foreach ($allUsers as $user) {
-        if (in_array($user["id"], $ids)) {
-            $usersById[] = $user;
+        if (isset($_GET["limit"])) {
+            $limit = $_GET["limit"];
+            $slicedUsers = array_slice($allUsers, 0, $limit);
+            sendJson($slicedUsers);
+            exit();
         }
-    } if(count($usersById)==0){
-        $json = json_encode(["message"=>"User does not exist"]);
-        sendJson($json, 400);
+
+
+        if (isset($_GET["id"])) {
+            $ids = explode(",", $_GET["id"]);
+            $usersById = [];
+
+            foreach ($allUsers as $user) {
+                if (in_array($user["id"], $ids)) {
+                    $usersById[] = $user;
+                }
+            } if(count($usersById)==0){
+                $json = json_encode(["message"=>"User does not exist"]);
+                sendJson($json, 400);
+            }
+            
+
+            sendJson($usersById);
+            exit();
+        }
+
+        //om det finns AGE i URL.
+        if(isset($_GET["age"])){
+            $age = $_GET["age"];
+            $ageArray = [];
+
+            foreach($allUsers as $user){
+                if ($user["age"] == $age){
+                    array_push($ageArray, $user);
+                }
+            } //om det inte finns användare i den åldern.
+            if (count($ageArray)== 0){
+                sendJson(["message" => "No users at this age."]);
+            } else { //annars skickar den ut alla med den specifika åldern.
+                sendJson($ageArray);
+            }
+        } else { //om inte det finns AGE, skickas alla användare ut ändå.
+            sendJson($allUsers);
+        }
     }
-    
-
-    sendJson($usersById);
-    exit();
-}
-
-sendJson($allUsers);
 ?>
