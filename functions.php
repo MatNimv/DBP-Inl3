6b03e907be82eaba93fbd717fb9bbe96b402f27f
olivialinnea <82369027+olivialinnea@users.noreply.github.com>
2021-11-19 11:52:41 +0100

    Skapade create.php + uppdaterade users.json, pets.json och functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,8 @@
 //denna funktion ska skicka ut en JSON till användaren.
 //får det som ett meddelande i en asso array, och även
 //en http-kod som visar om det gått rätt eller fel.
-function sendJson($data, $statusCode = 200){
+function sendJson($data, $statusCode = 200)
+{
     //innehållet som skickas är i JSON
     header("Content-Type: application/json");
 
@@ -18,8 +19,9 @@ function sendJson($data, $statusCode = 200){
 }
 
 //läsa in JSON-fil, konvertera till asso array
-function loadJSON($fileName){
-    if (file_exists($fileName) == true){ 
+function loadJSON($fileName)
+{
+    if (file_exists($fileName) == true) {
         //om filen finns (är true) - då
         //gör JSONdatan till en asso array
         $data = file_get_contents("$fileName");
@@ -33,21 +35,37 @@ function loadJSON($fileName){
 //spara en asso array som i en fil, 
 //$fileName är filen vi vill lägga in det nya i.
 //$data är objektet som sparas in i objektet.
-function saveJson($fileName, $data){
+function saveJson($fileName, $data)
+{
     //sätter in i $fileName: datan i fint format
-    if( //om file_put_contents blir false skickas false med.
+    if ( //om file_put_contents blir false skickas false med.
         file_put_contents(
-        $fileName,
-        json_encode($data, JSON_PRETTY_PRINT)
-    ) == false){
+            $fileName,
+            json_encode($data, JSON_PRETTY_PRINT)
+        ) == false
+    ) {
         return false;
     } else {
         return true;
     }
 }
 
+//Hittar det högsts id:et
+function theHighestId($array)
+{
+    $userID = 0;
+    foreach ($array as $obj) {
+        if ($obj["id"] > $userID) {
+            $userID = $obj["id"];
+        }
+    }
+    $userID = $userID + 1;
+    return $userID;
+}
+
 //en funktion som "skriver ut" värdet. Funkar bara i webbläsaren.
-function inspect($variable){
+function inspect($variable)
+{
     echo '<pre>';
     echo var_dump($variable);
     echo '</pre>';
